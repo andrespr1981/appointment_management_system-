a0d7dcb9faf816c82c8e222b130dab6a60d8d36f
andrespr1981 <andrespardorubalcaba@gmail.com>
2026-03-06 18:09:14 -0800

    project structure

---
+++ components/navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+//El nombre va con mayuscula para que se reconozca como un componente
+export default function Navbar() {
+    return (
+        <div>navbar</div>
+    )
+}
